ecc06a545fd2f8a89c830873a827dcb46c318017
That WP Developer <thatwpdeveloper@gmail.com>
2018-12-27 21:24:54 +0000

    Added files

---
+++ includes/class-activator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/thatwpdeveloper
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ */
+namespace Plugin_Name;
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     That WP Developer <thatwpdeveloper@gmail.com>
+ */
+class Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-deactivator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/thatwpdeveloper
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ */
+namespace Plugin_Name;
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     That WP Developer <thatwpdeveloper@gmail.com>
+ */
+class Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
